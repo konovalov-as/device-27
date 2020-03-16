8e29f09939c12375365446cd2406191b84ce4ad6
Konovalov Alexandr <konovalov_a.s@mail.ru>
2020-03-16 15:36:34 +0500

    Исправлены состояния кнопок с желтой полоской

--- js/script.js
+++ js/script.js
@@ -6,22 +6,42 @@ let openContactsWriteToUs = document.querySelector(".contacts__write-to-us");
 let modalWriteToUs = document.querySelector(".modal__write-to-us");
 let modalCloseWriteToUs = document.querySelector(".modal__close-form-write-to-us");
 
-openContactsMapInteractive.addEventListener("click", function(evt) {
-    modalMap.classList.add("modal--show");
-})
+let submitFormWriteToUs = modalWriteToUs.querySelector(".modal__submit-form-write-to-us");
+let yourName = modalWriteToUs.querySelector(".modal__form-your-name-input");
+let yourEmail = modalWriteToUs.querySelector(".modal__form-your-email-input");
 
-modalCloseMap.addEventListener("click", function(evt) {
+openContactsMapInteractive.addEventListener("click", function (evt) {
+    modalMap.classList.add("modal--show");
+});
+
+modalCloseMap.addEventListener("click", function (evt) {
     modalMap.classList.remove("modal--show");
 });
 
-
-openContactsWriteToUs.addEventListener("click", function(evt) {
+openContactsWriteToUs.addEventListener("click", function (evt) {
     evt.preventDefault();
     modalWriteToUs.classList.add("modal--show");
 });
 
-modalCloseWriteToUs.addEventListener("click", function(evt) {
+modalCloseWriteToUs.addEventListener("click", function (evt) {
     modalWriteToUs.classList.remove("modal--show");
-})
+});
 
-console.log(modalCloseWriteToUs);
+modalWriteToUs.addEventListener("submit", function (evt) {
+    if (!yourName.value) {
+        evt.preventDefault();
+        yourName.classList.add("modal__form-your-name-input--error");
+    }
+    if (!yourEmail.value) {
+        evt.preventDefault();
+        yourEmail.classList.add("modal__form-your-email-input--error");
+    }
+});
+
+yourName.addEventListener("input", function () {
+    yourName.classList.remove("modal__form-your-name-input--error");
+});
+
+yourEmail.addEventListener("input", function () {
+    yourEmail.classList.remove("modal__form-your-email-input--error");
+});
